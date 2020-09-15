1ce10908729039d69abda1b3d5fee4abfd16be8d
phatdroid94 <phatdroid94@gmail.com>
2020-09-15 18:38:41 +0700

    dang ky va dang nhap

--- app/src/main/java/com/example/firebaseauthentication02062020/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthentication02062020/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.firebaseauthentication02062020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText mEdtEmail,mEdtPass;
-    Button mBtnDangky,mBtnDangnhap,mBtnThongTin,mBtnCapNhatThongTin,mBtnCapNhatMatKhau,mBtnXacThucEmail;
+    EditText mEdtEmail, mEdtPass;
+    Button mBtnDangky, mBtnDangnhap, mBtnThongTin, mBtnCapNhatThongTin, mBtnCapNhatMatKhau, mBtnXacThucEmail;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +34,51 @@ public class MainActivity extends AppCompatActivity {
         mBtnCapNhatThongTin = findViewById(R.id.buttonCapnhat);
         mBtnCapNhatMatKhau = findViewById(R.id.buttonUpdatePassword);
         mBtnXacThucEmail = findViewById(R.id.buttonVerification);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mBtnDangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEdtEmail.getText().toString();
+                String password = mEdtPass.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Dang ky thanh cong", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Dang ky that bai", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+        mBtnDangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEdtEmail.getText().toString();
+                String password = mEdtPass.getText().toString();
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d("BBB",mAuth.getCurrentUser().getEmail());
+                                    Toast.makeText(MainActivity.this, "Dang nhap thanh cong", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("BBB",mAuth.getCurrentUser().getEmail());
     }
 }
