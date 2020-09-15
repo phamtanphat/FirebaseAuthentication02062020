95259e69a7520b9d8bd1cf4414f04d10c40c6185
phatdroid94 <phatdroid94@gmail.com>
2020-09-15 19:32:40 +0700

    admob

--- app/src/main/java/com/example/firebaseauthentication02062020/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthentication02062020/MainActivity.java
@@ -11,6 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -23,11 +28,20 @@ public class MainActivity extends AppCompatActivity {
     EditText mEdtEmail, mEdtPass;
     Button mBtnDangky, mBtnDangnhap, mBtnThongTin, mBtnCapNhatThongTin, mBtnCapNhatMatKhau, mBtnXacThucEmail;
     FirebaseAuth mAuth;
-
+    private AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         mEdtPass = findViewById(R.id.edittextPassword);
         mEdtEmail = findViewById(R.id.edittextEmail);
